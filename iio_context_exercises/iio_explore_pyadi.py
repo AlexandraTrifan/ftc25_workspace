92f6d57a16512a3fecd18895e07ae41a5cf7dfc2
AlexandraTrifan <Alexandra.Trifan@analog.com>
2025-11-06 02:12:11 -0500

    pyadi-iio ctx exercise: read gain before and after
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- iio_context_exercises/iio_explore_pyadi.py
+++ iio_context_exercises/iio_explore_pyadi.py
@@ -7,5 +7,6 @@ sdr.gain_control_mode_chan0 = 'manual'
 print("RX gain control mode: " + sdr.gain_control_mode_chan0)
 
 # Set the RX hardware gain for Pluto
+print("RX gain before: " + str(sdr.rx_hardwaregain_chan0))
 sdr.rx_hardwaregain_chan0 = 10
-print("RX gain : " + str(sdr.rx_hardwaregain_chan0))
+print("RX gain after: " + str(sdr.rx_hardwaregain_chan0))
